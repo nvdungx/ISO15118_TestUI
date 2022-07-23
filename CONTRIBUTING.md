5eec0a274cfaf8b40931858f369b1be1955ff8c5
Dungnv <nvdung694@gmail.com>
2022-07-23 20:09:49 +0700

    change server base url, add script start server

---
+++ CONTRIBUTING.md
@@ -0,0 +1,1 @@
+this repos is the implmentation of a simple testUI(with django, Vuejs), please feel free to refer to it
--- testui/settings.py
+++ testui/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9+zlvo+%xw3!212fp0^pvptno&jt5$s)h@7=u&vli00vox815%'
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
